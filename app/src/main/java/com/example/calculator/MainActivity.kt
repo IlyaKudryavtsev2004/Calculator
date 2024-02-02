9b8a831680262da08e38866cf69ead592f0cab08
ilyushenm <Qaiscastor@gmail.com>
2024-02-02 10:33:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(androidx.core.R.layout.ime_base_split_test_activity)
+    }
+}
